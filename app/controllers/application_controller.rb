5433b564e087779f3c622dd68d755f3a5ebbab98
Patrick McCombs <pa.mccombs1@gmail.com>
2019-04-25 17:42:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,12 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     
-    
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
 
   end
 

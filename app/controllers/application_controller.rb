e731f8933d9dcb68e3b79ff78b3e13b27d07ae05
Patrick McCombs <pa.mccombs1@gmail.com>
2019-04-25 18:01:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+    
+     @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
 
   end
 
